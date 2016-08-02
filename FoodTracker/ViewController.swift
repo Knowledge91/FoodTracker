a9495c5051f96c320224095042982de6d08a3266
Knowledge <dirkhornung91@gmail.com>
2016-08-02 13:56:05 +0200

    Deleted MemoryWarning() in ViewController

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -13,14 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print('Crazy this will be my first commit!')
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 

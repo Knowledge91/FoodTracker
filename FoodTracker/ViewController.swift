f5b45cf1939ad0ed88e3b7da94ce07f69fa461a3
Knowledge <dirkhornung91@gmail.com>
2016-08-02 13:23:48 +0200

    Crazy this is my first commit!

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print('Crazy this will be my first commit!')
     }
 
     override func didReceiveMemoryWarning() {

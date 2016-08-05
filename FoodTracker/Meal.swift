20f490fbe39320e045b28a8ae967518168dd9cf7
Knowledge <dirkhornung91@gmail.com>
2016-08-05 13:04:29 +0200

    Define Your Data Model

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,30 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Dirk Hornung on 5/8/16.
+//  Copyright © 2016 Dirk Hornung. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // Initialize store properties.
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+}
